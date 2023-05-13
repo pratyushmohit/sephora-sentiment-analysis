668f1ff3be7e5fd0e32713274f4b7a749c14489e
pratyushmohit <pratyushmohit226@gmail.com>
2023-05-13 15:30:46 +0530

    updated run.sh and main.py

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 echo "\nStarting Sephora Sentiment Analysis..."
 
-python -m uvicorn sentiment_analysis.main:app --reload
+python -m uvicorn sentiment_analysis.main:app --reload --host=0.0.0.0 --port=8000
--- sentiment_analysis/main.py
+++ sentiment_analysis/main.py
@@ -6,3 +6,7 @@ from fastapi import Response
 def status():
     json_str = json.dumps({"status": "Running"}, indent=4, default=str)
     return Response(content=json_str, media_type='application/json')
+
+@app.get('/')
+async def index():
+    return {"message": "Sephora Sentiment Analysis"}

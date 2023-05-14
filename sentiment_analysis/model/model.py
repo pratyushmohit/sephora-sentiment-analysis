44a94fad75cee0f6f9ed837b7e7702d2359afbc5
pratyushmohit <pratyushmohit226@gmail.com>
2023-05-14 21:20:15 +0530

    formatted with autopep8

--- sentiment_analysis/model/model.py
+++ sentiment_analysis/model/model.py
@@ -6,41 +6,45 @@ from keras.optimizers import Nadam
 
 
 class SentimentAnalysisModel():
-    def __init__(self, config):
-        self.config = config
+    def __init__(self, data_config, model_config):
+        self.data_config = data_config
+        self.model_config = model_config
 
     def model(self):
         tf.keras.backend.clear_session()
 
-        input_layer_1 = Input(shape=(max_len_review_text,))
-        embedding_1 = Embedding(input_dim=size_of_vocab_review_text, output_dim=300, weights=[embedding_matrix_review_text], input_length=max_len_review_text,
+        input_layer_1 = Input(
+            shape=(self.data_config["review_text"]["max_len"],))
+        embedding_1 = Embedding(input_dim=self.data_config["review_text"]["size_of_vocab"], output_dim=300, weights=[embedding_matrix_review_text], input_length=max_len_review_text,
                                 trainable=False)(input_layer_1)
         lstm = LSTM(100, return_sequences=True)(embedding_1)
         flatten_1 = Flatten()(lstm)
 
-        input_layer_2 = Input(shape=(max_len_review_title,))
-        embedding_2 = Embedding(input_dim=size_of_vocab_review_title, output_dim=50, weights=[embedding_matrix_review_title], input_length=max_len_review_title, 
+        input_layer_2 = Input(
+            shape=(self.data_config["review_title"]["max_len"],))
+        embedding_2 = Embedding(input_dim=self.data_config["review_title"]["size_of_vocab"], output_dim=50, weights=[embedding_matrix_review_title], input_length=max_len_review_title,
                                 trainable=True)(input_layer_2)
         flatten_2 = Flatten()(embedding_2)
 
         input_layer_3 = Input(shape=(3,))
-        embedding_3 = Embedding(input_dim=project_grade_category_size_of_vocabulary, output_dim=2, input_length=1, trainable=True)(input_layer_3)
+        embedding_3 = Embedding(input_dim=project_grade_category_size_of_vocabulary,
+                                output_dim=2, input_length=1, trainable=True)(input_layer_3)
         flatten_3 = Flatten()(embedding_3)
 
         input_layer_4 = Input(shape=(clean_categories_max_len,))
-        embedding_4 = Embedding(input_dim=clean_categories_size_of_vocabulary, output_dim=2, input_length=clean_categories_max_len, 
+        embedding_4 = Embedding(input_dim=clean_categories_size_of_vocabulary, output_dim=2, input_length=clean_categories_max_len,
                                 trainable=True)(input_layer_4)
         flatten_4 = Flatten()(embedding_4)
 
         input_layer_5 = Input(shape=(clean_subcategories_max_len,))
-        embedding_5 = Embedding(input_dim=clean_subcategories_size_of_vocabulary, output_dim=2, input_length=clean_subcategories_max_len, 
+        embedding_5 = Embedding(input_dim=clean_subcategories_size_of_vocabulary, output_dim=2, input_length=clean_subcategories_max_len,
                                 trainable=True)(input_layer_5)
         flatten_5 = Flatten()(embedding_5)
 
         input_layer_6 = Input(shape=(1,))
-        embedding_6 = Embedding(input_dim=teacher_prefix_size_of_vocabulary, output_dim=2, input_length=1, trainable=True)(input_layer_6)
+        embedding_6 = Embedding(input_dim=teacher_prefix_size_of_vocabulary,
+                                output_dim=2, input_length=1, trainable=True)(input_layer_6)
         flatten_6 = Flatten()(embedding_6)
 
         input_layer_7 = Input(shape=(2,))
         dense = Dense(50, activation='relu')(input_layer_7)
-
--- sentiment_analysis/pipeline/pipeline.py
+++ sentiment_analysis/pipeline/pipeline.py
@@ -21,49 +21,64 @@ class Pipeline(BasePipeline):
             output = preprocessor.ingest()
             x_train, x_test, y_train, y_test = preprocessor.split_data()
 
-            review_text = preprocessor.preprocess_text_batch(x_train, "review_text")
-            review_title = preprocessor.preprocess_text_batch(x_train, "review_title")
-            brand_name = preprocessor.preprocess_categorical(x_train, "brand_name")
+            review_text = preprocessor.preprocess_text_batch(
+                x_train, "review_text")
+            review_title = preprocessor.preprocess_text_batch(
+                x_train, "review_title")
+            brand_name = preprocessor.preprocess_categorical(
+                x_train, "brand_name")
             price_usd = preprocessor.preprocess_numerical(x_train, "price_usd")
             y = preprocessor.preprocess_y(y_train)
 
             x = pd.DataFrame({"review_text": review_text,
-                                    "review_title": review_title,
-                                    "brand_name": brand_name,
-                                    "price_usd": price_usd})
-            
+                              "review_title": review_title,
+                              "brand_name": brand_name,
+                              "price_usd": price_usd})
+
             preprocessor.save_data(x, "x_train_preprocessed.csv")
             preprocessor.save_data(y, "y_train.npy")
         else:
-            x = pd.read_csv("sentiment_analysis\\data\preprocessed_data\\x_train_preprocessed.csv", 
-                                            index_col="Unnamed: 0")
+            x = pd.read_csv("sentiment_analysis\\data\preprocessed_data\\x_train_preprocessed.csv",
+                            index_col="Unnamed: 0")
             x.reset_index(inplace=True, drop=True)
 
-        #To do: Try to generalize the following code to account for any type of feature and for train set or test
+        # To do: Try to generalize the following code to account for any type of feature and for train set or test
         data_config = {"review_text": {},
                        "review_title": {},
                        "brand_name": {}}
-        
-        tokenizer_review_text, vocab_review_text, size_of_vocab_review_text = preprocessor.tokenization(x["review_text"])
-        embedding_matrix_review_text = preprocessor.glove_embedding(vocab_review_text, size_of_vocab_review_text, 300)
-        max_len_review_text = max([len(datapoint.split()) for datapoint in x["review_text"]])
-        sequences_review_text = tokenizer_review_text.texts_to_sequences(x["review_text"])
-        padded_sequences_review_text = preprocessor.padding(sequences_review_text, max_len_review_text)
+
+        tokenizer_review_text, vocab_review_text, size_of_vocab_review_text = preprocessor.tokenization(
+            x["review_text"])
+        embedding_matrix_review_text = preprocessor.glove_embedding(
+            vocab_review_text, size_of_vocab_review_text, 300)
+        max_len_review_text = max([len(datapoint.split())
+                                  for datapoint in x["review_text"]])
+        sequences_review_text = tokenizer_review_text.texts_to_sequences(
+            x["review_text"])
+        padded_sequences_review_text = preprocessor.padding(
+            sequences_review_text, max_len_review_text)
         data_config["review_text"]["max_len"] = max_len_review_text
         data_config["review_text"]["size_of_vocab"] = size_of_vocab_review_text
 
-
-        tokenizer_review_title, vocab_review_title, size_of_vocab_review_title = preprocessor.tokenization(x["review_title"])
-        embedding_matrix_review_title = preprocessor.glove_embedding(vocab_review_title, size_of_vocab_review_title, 50)
-        max_len_review_title = max([len(datapoint.split()) for datapoint in x["review_title"]])
-        sequences_review_title = tokenizer_review_title.texts_to_sequences(x["review_title"])
-        padded_sequences_review_title = preprocessor.padding(sequences_review_title, max_len_review_title)
+        tokenizer_review_title, vocab_review_title, size_of_vocab_review_title = preprocessor.tokenization(
+            x["review_title"])
+        embedding_matrix_review_title = preprocessor.glove_embedding(
+            vocab_review_title, size_of_vocab_review_title, 50)
+        max_len_review_title = max([len(datapoint.split())
+                                   for datapoint in x["review_title"]])
+        sequences_review_title = tokenizer_review_title.texts_to_sequences(
+            x["review_title"])
+        padded_sequences_review_title = preprocessor.padding(
+            sequences_review_title, max_len_review_title)
         data_config["review_title"]["max_len"] = max_len_review_title
         data_config["review_title"]["size_of_vocab"] = size_of_vocab_review_title
 
-        tokenizer_brand_name, vocab_brand_name, size_of_vocab_brand_name = preprocessor.tokenization(x["brand_name"])
-        max_len_brand_name = max([len(datapoint.split()) for datapoint in x["brand_name"]])
-        sequences_brand_name = np.array(tokenizer_brand_name.texts_to_sequences(x['brand_name']), dtype=object)
+        tokenizer_brand_name, vocab_brand_name, size_of_vocab_brand_name = preprocessor.tokenization(
+            x["brand_name"])
+        max_len_brand_name = max([len(datapoint.split())
+                                 for datapoint in x["brand_name"]])
+        sequences_brand_name = np.array(
+            tokenizer_brand_name.texts_to_sequences(x['brand_name']), dtype=object)
         data_config["brand_name"]["max_len"] = max_len_brand_name
         data_config["brand_name"]["size_of_vocab"] = size_of_vocab_brand_name
 
@@ -73,16 +88,21 @@ class Pipeline(BasePipeline):
 
         price_usd = x["price_usd"]
 
-        preprocessor.save_data(embedding_matrix_review_text, "embedding_matrix_review_text.npy")
-        preprocessor.save_data(padded_sequences_review_text, "padded_sequences_review_text.npy")
-        preprocessor.save_data(embedding_matrix_review_title, "embedding_matrix_review_title.npy")
-        preprocessor.save_data(padded_sequences_review_title, "padded_sequences_review_title.npy")
-        preprocessor.save_data(sequences_brand_name, "sequences_brand_name.npy")
+        preprocessor.save_data(embedding_matrix_review_text,
+                               "embedding_matrix_review_text.npy")
+        preprocessor.save_data(padded_sequences_review_text,
+                               "padded_sequences_review_text.npy")
+        preprocessor.save_data(embedding_matrix_review_title,
+                               "embedding_matrix_review_title.npy")
+        preprocessor.save_data(padded_sequences_review_title,
+                               "padded_sequences_review_title.npy")
+        preprocessor.save_data(sequences_brand_name,
+                               "sequences_brand_name.npy")
         preprocessor.save_data(price_usd, "price_usd.npy")
 
-        output = json.dumps({"status": "Preprocessing Pipeline successfully executed!"}, indent=4, default=str)
+        output = json.dumps(
+            {"status": "Preprocessing Pipeline successfully executed!"}, indent=4, default=str)
         return output
-    
-    
+
     def model_pipeline(self):
         pass
